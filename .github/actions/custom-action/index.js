2d290338fa1c3b79b6f2e8472b465c50d992bdc4
Josh Black <josh@josh.black>
2019-10-16 18:40:24 -0500

    update action

--- .github/actions/custom-action/index.js
+++ .github/actions/custom-action/index.js
@@ -4,12 +4,45 @@ const github = require('@actions/github');
 const core = require('@actions/core');
 
 async function run() {
-  console.log(process.env.GITHUB_REF);
-  // const token = core.getInput('GITHUB_TOKEN');
+  const { GITHUB_REF, GITHUB_REPOSITORY } = process.env;
+  const token = core.getInput('GITHUB_TOKEN');
+  const octokit = new github.GitHub(token);
+  const [owner, repo] = GITHUB_REPOSITORY.split('/');
 
-  // const octokit = new github.GitHub(token);
+  const { data } = await octokit.repos.createDeployment({
+    owner,
+    repo,
+    ref: GITHUB_REF,
+    environment: 'preview',
+    description: 'test description',
+  });
 
-  // await octokit.repos.createDeployment
+  console.log(data.id);
+  // await octokit.repos.createDeploymentStatus({
+    // owner,
+    // repo,
+    // id: data.id,
+    // // 'error', 'failure', 'inactive', 'in_progress', 'queued pending',
+    // // 'success'
+    // state: 'in_progress',
+  // });
+
+  // await sleep(10000);
+
+  // await octokit.repos.createDeploymentStatus({
+    // owner,
+    // repo,
+    // id: data.id,
+    // // 'error', 'failure', 'inactive', 'in_progress', 'queued pending',
+    // // 'success'
+    // state: 'success',
+  // });
+}
+
+function sleep(ms = 1000) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
 }
 
 run().catch(error => {
