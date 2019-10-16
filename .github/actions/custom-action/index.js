232e6d9a8b40101d5beb3e49c94c705c5a04909c
Josh Black <josh@josh.black>
2019-10-16 18:31:01 -0500

    update action

--- .github/actions/custom-action/index.js
+++ .github/actions/custom-action/index.js
@@ -4,7 +4,7 @@ const github = require('@actions/github');
 const core = require('@actions/core');
 
 async function run() {
-  console.log(core.getInput('GITHUB_REF'));
+  console.log(process.env.GITHUB_REF);
   // const token = core.getInput('GITHUB_TOKEN');
 
   // const octokit = new github.GitHub(token);
