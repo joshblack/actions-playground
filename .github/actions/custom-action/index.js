08a5b5555da6b79e77d8d886513c5dba63a1589d
Josh Black <josh@josh.black>
2019-10-16 17:52:37 -0500

    update action

--- .github/actions/custom-action/index.js
+++ .github/actions/custom-action/index.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('hi');
+const github = require('@actions/github');
+const core = require('@actions/core');
+
+async function run() {
+  const token = core.getInput('GITHUB_TOKEN');
+  const octokit = new github.GitHub(token);
+
+  const { data } = await octokit.pulls.list({
+    owner: 'joshblack',
+    repo: 'actions-playground',
+  });
+
+  console.log(data);
+}
+
+run().catch(error => {
+  console.log(error);
+  process.exit(1);
+});
