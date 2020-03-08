9191ba87e46f538234f7bfe25c61815714759f58
Josh Black <josh@josh.black>
2020-03-08 15:09:25 -0500

    chore: check-in work

--- .github/actions/reviewer/index.js
+++ .github/actions/reviewer/index.js
@@ -10,12 +10,51 @@ async function run() {
     previews: ['flash-preview'],
   });
 
-  console.log(context);
+  const { pull_request: pullRequest } = context.payload;
+  if (!pullRequest) {
+    throw new Error(`Unable to determine pull request from context`);
+  }
+
+  const { id, labels, number, repository, state, draft, user } = pullRequest;
+
+  // We only want to work with Pull Requests marked as open
+  if (state !== 'open') {
+    return;
+  }
+
+  // We only want to work with Pull Requests that are not draft PRs
+  if (draft) {
+    return;
+  }
+
+  const { name, owner } = repository;
+  console.log(name);
+  console.log(owner);
+  console.log(user);
+
+  // list reviewers
+  // octokit.pulls.listReviews({ owner, repo, pull_number });
+
+  // list labels for review
+
+  // check if reviewer is collaborator
+  // octokit.repos.checkCollaborator({ owner, repo, username });
+
+  // Add label to pull (issue)
+  // octokit.issues.addLabels({ owner, repo, issue_number, labels: ['label-name'] });
+
+  // Get all labels?
 
   // 1. Get the Pull Request
   // 2. Get the reviewers for the Pull Request
   // 3. Get the labels for the Pull Request
   // 4. Check reviewers against labels
+
+  // Auto-merge?
+  // octokit.pulls.merge({ owner, repo, pull_number });
+
+  // Auto-update?
+  // octokit.pulls.updateBranch({ owner, repo, pull_number });
 }
 
 run().catch(error => {
