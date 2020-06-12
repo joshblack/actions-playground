86bbabb07cd92642441f80ff8a9fd24471a27e34
Josh Black <josh@josh.black>
2020-06-11 20:43:29 -0500

    add labels for ocmment

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -3,14 +3,28 @@
 const github = require('@actions/github');
 const core = require('@actions/core');
 
+const labels = new Map([
+  ['waiting-on-maintainer', 'waiting on maintainer response'],
+  ['waiting-on-author', `waiting on author's response`],
+]);
+
 async function run() {
   const { context } = github;
   const token = core.getInput('GITHUB_TOKEN', {
     required: true,
   });
   const octokit = new github.GitHub(token);
-  console.log(context.payload);
-  // const { action, issue, repository } = context.payload;
+  const { issue, repository } = context.payload;
+
+  await octokit.issues.addLabels({
+    owner: repository.owner.login,
+    repo: repository.name,
+    issue_number: issue.number,
+    labels: [
+      labels.get('waiting-on-maintainer'),
+      labels.get('waiting-on-author'),
+    ],
+  });
 }
 
 run().catch(error => {
