97f155de703afa96670b765f4fc4ee2a7ebc3b73
Josh Black <josh@josh.black>
2020-06-25 20:36:26 -0500

    chore: sync actions

--- .github/actions/comment/index.js
+++ .github/actions/comment/index.js
@@ -3,27 +3,71 @@
 const github = require('@actions/github');
 const core = require('@actions/core');
 
+// opened
+// closed
+// commented
+// labeled
+// unlabeled
+
 async function run() {
+  const enabled = core.getInput('enabled') || true;
+  if (!enabled) {
+    core.info('Action is not enabled. Exiting');
+    return;
+  }
+
   const { context } = github;
   const token = core.getInput('GITHUB_TOKEN', {
     required: true,
   });
   const octokit = new github.GitHub(token);
   const { action, issue, repository } = context.payload;
-  const needsTriageLabel = 'needs triage';
 
-  if (action === 'opened') {
-    const hasTriageLabel = issue.labels.find(label => {
-      return label.name === needsTriageLabel;
+  if (issue.pull_request) {
+    core.info('Action ran for a Pull Request that does not apply. Exiting');
+    return;
+  }
+  const plugins = [addTriageLabel, addWaitingForResponse];
+
+  for (const plugin of plugins) {
+    await plugin(context, octokit);
+  }
+}
+
+const needsTriageLabel = 'needs triage';
+
+async function addTriageLabel(context, octokit) {
+  const { action, issue, repository } = context.payload;
+  if (action !== 'opened') {
+    return;
+  }
+
+  const hasTriageLabel = issue.labels.find(label => {
+    return label.name === needsTriageLabel;
+  });
+
+  if (!hasTriageLabel) {
+    await octokit.issues.addLabels({
+      owner: repository.owner.login,
+      repo: repository.name,
+      issue_number: issue.number,
+      labels: [needsTriageLabel],
     });
-    if (!hasTriageLabel) {
-      await octokit.issues.addLabels({
-        owner: repository.owner.login,
-        repo: repository.name,
-        issue_number: issue.number,
-        labels: [needsTriageLabel],
-      });
-    }
+  }
+}
+
+async function addWaitingForResponse(context, octokit) {
+  const { action, repository } = context.payload;
+  if (action !== 'commented') {
+    return;
+  }
+
+  console.log(context.payload);
+
+  const hasTriageLabel = issue.labels.find(label => {
+    return label.name === needsTriageLabel;
+  });
+  if (!hasTriageLabel) {
     return;
   }
 }
