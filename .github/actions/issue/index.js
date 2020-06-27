a65025330422e855a14baec97a117713c0195bd0
Josh Black <josh@josh.black>
2020-06-27 17:03:44 -0500

    chore: sync action

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -8,23 +8,83 @@ const labels = new Map([
   ['waiting-on-author', `waiting on author's response`],
 ]);
 
+// opened
+// closed
+// commented
+// labeled
+// unlabeled
+
 async function run() {
+  const enabled = core.getInput('enabled') || true;
+  if (!enabled) {
+    core.info('Action is not enabled. Exiting');
+    return;
+  }
+
   const { context } = github;
   const token = core.getInput('GITHUB_TOKEN', {
     required: true,
   });
   const octokit = new github.GitHub(token);
-  const { issue, repository } = context.payload;
+  const { issue } = context.payload;
 
-  await octokit.issues.addLabels({
-    owner: repository.owner.login,
-    repo: repository.name,
-    issue_number: issue.number,
-    labels: [
-      labels.get('waiting-on-maintainer'),
-      labels.get('waiting-on-author'),
-    ],
+  if (issue.pull_request) {
+    core.info('Action ran for a Pull Request that does not apply. Exiting');
+    return;
+  }
+
+  const plugins = [addTriageLabel, addWaitingForResponse];
+  for (const plugin of plugins) {
+    await plugin(context, octokit);
+  }
+
+  // await octokit.issues.addLabels({
+  // owner: repository.owner.login,
+  // repo: repository.name,
+  // issue_number: issue.number,
+  // labels: [
+  // labels.get('waiting-on-maintainer'),
+  // labels.get('waiting-on-author'),
+  // ],
+  // });
+}
+
+const needsTriageLabel = 'needs triage';
+
+async function addTriageLabel(context, octokit) {
+  const { action, issue, repository } = context.payload;
+  if (action !== 'opened') {
+    return;
+  }
+
+  const hasTriageLabel = issue.labels.find(label => {
+    return label.name === needsTriageLabel;
   });
+
+  if (!hasTriageLabel) {
+    await octokit.issues.addLabels({
+      owner: repository.owner.login,
+      repo: repository.name,
+      issue_number: issue.number,
+      labels: [needsTriageLabel],
+    });
+  }
+}
+
+async function addWaitingForResponse(context, octokit) {
+  const { action } = context.payload;
+  if (action !== 'commented') {
+    return;
+  }
+
+  console.log(context.payload);
+
+  const hasTriageLabel = issue.labels.find(label => {
+    return label.name === needsTriageLabel;
+  });
+  if (!hasTriageLabel) {
+    return;
+  }
 }
 
 run().catch(error => {
